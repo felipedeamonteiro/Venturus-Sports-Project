52283bf27262e76a7b90d9fe716ead6b504cc165
Felipe de Alcântara Monteiro <36462206+SquallxX@users.noreply.github.com>
2019-03-23 14:21:10 -0300

    Added Servidor and build folders
    There are the folders Servidor Node, containing a node server, and build, where has all the files of the project to be run in a browser.

---
+++ Servidor Node/build/precache-manifest.2dc2222697050b45926040cd41328c24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "43ac06318e47f9ff3666",
+    "url": "/static/css/main.924673e9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a74ae5d28f84c10fa8b9",
+    "url": "/static/js/1.a74ae5d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "43ac06318e47f9ff3666",
+    "url": "/static/js/main.43ac0631.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a74ae5d28f84c10fa8b9",
+    "url": "/static/css/1.e3d216c6.chunk.css"
+  },
+  {
+    "revision": "83004c0d00f44f6ce60a2f5c5dc1522b",
+    "url": "/index.html"
+  }
+];
